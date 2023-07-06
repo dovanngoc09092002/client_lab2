791e97ea0311235d0fb596178b7bf76f042d1540
dovanngoc09092002 <phamminhvien9922@gmail.com>
2023-07-06 15:23:40 +0700

    update5

--- src/app/features/prompt/components/promt-list/prompt-list.component.ts
+++ src/app/features/prompt/components/promt-list/prompt-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+declare const FB: any;
 @Component({
   // standalone: true,
   // imports: [RouterModule, MessageListComponent, MessageDetailComponent],
@@ -8,5 +9,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./prompt-list.component.scss'],
 })
 export class PromptListComponent {
+  title = 'login-page';
 
+  isLogin = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    (window as any).fbAsyncInit = () => {
+      FB.init({
+        appId: '1569535140204107',
+        cookie: true,
+        xfbml: true,
+        version: 'v17.0',
+      });
+
+      FB.getLoginStatus((response: any) => {
+        statusChangeCallback(response);
+      });
+      FB.AppEvents.logPageView();
+    };
+
+    const statusChangeCallback = (response: any) => {
+      console.log('statusChangeCallback');
+      console.log(response);
+      if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        this.isLogin = true;
+        console.log('login: ', this.isLogin);
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function (response: any) {
+          console.log('Successful login for: ' + response.name);
+          document.getElementById('status')!.innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+        });
+      } else {
+        document.getElementById('status')!.innerHTML =
+          'Please log ' + 'into this app.';
+      }
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0] as HTMLElement;
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s) as HTMLScriptElement;
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode!.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  }
+
+  logout() {
+    FB.logout((response: any) => {
+      console.log('Đăng xuất thành công');
+      this.router.navigate(['/']);
+    });
+  }
 }
--- src/app/features/prompt/prompt.module.ts
+++ src/app/features/prompt/prompt.module.ts
@@ -1,50 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { PromptEditorComponent } from './components/prompt-editor/prompt-editor.component';
 import { PromptListComponent } from './components/promt-list/prompt-list.component';
 import { PromptRouting } from './prompt-routing.module';
+import { Router } from '@angular/router';
 declare const FB: any;
 @NgModule({
   declarations: [PromptEditorComponent, PromptListComponent],
   imports: [PromptRouting],
 })
-export class PromptModule {
-  constructor() {
-    
+export class PromptModule implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
-
-  // connectFacebook() {
-  //   (window as any).fbAsyncInit = function () {
-  //     FB.init({
-  //       appId: '616786400428551',
-  //       cookie: true,
-  //       xfbml: true,
-  //       version: 'v17.0',
-  //     });
-
-  //     FB.AppEvents.logPageView();
-  //   };
-
-  //   (function (d, s, id) {
-  //     var js,
-  //       fjs = d.getElementsByTagName(s)[0] as HTMLElement;
-  //     if (d.getElementById(id)) {
-  //       return;
-  //     }
-  //     js = d.createElement(s) as HTMLScriptElement;
-  //     js.id = id;
-  //     js.src = 'https://connect.facebook.net/en_US/sdk.js';
-  //     fjs.parentNode!.insertBefore(js, fjs);
-  //   })(document, 'script', 'facebook-jssdk');
-  // }
-  // // getLoginStatus(){
-
-  // //   FB.getLoginStatus(function(response: any) {
-  // //     statusChangeCallback(response);
-  // // })
-  // // }
-  // checkLoginState() {
-  //   FB.getLoginStatus(function (response: any) {
-  //     console.log(response);
-  //   });
-  // }
+  
 }
